59d78def29eb63135195ac3dd9a1159dcbd9400f
vuducsaobien <vuducsaobien95@gmail.com>
2023-04-30 15:09:41 +0700

    update DB migration

---
+++ database/migrations/2023_04_30_000000_create_category_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        // Insert some stuff
+        DB::table('category')->insert(['name' => 'category - name 1']);
+        DB::table('category')->insert(['name' => 'category - name 2']);
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
