0fccfb6b86d8b578d19d9e1b237d143af82ca549
vuducsaobien <vuducsaobien95@gmail.com>
2023-07-02 10:05:18 +0700

    Add Auth Laravel

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CachesController;
 use App\Http\Controllers\DatabaseController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,14 +17,25 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('index')->middleware('hidePass');
-Route::get('/queue', [HomeController::class, 'homeQueue'])->name('homeQueue');
-Route::get('/event/{id}', [HomeController::class, 'evenListeneerShip'])->name('evenListeneerShip');
-Route::get('/cache/{id}', [HomeController::class, 'cacheRedis'])->name('cacheRedis');
-Route::get('/cache', [CachesController::class, 'index'])->name('cacheRedisIndex');
+// Route::get('/', [HomeController::class, 'index'])->name('index')->middleware('hidePass');
+// Route::get('/queue', [HomeController::class, 'homeQueue'])->name('homeQueue');
+// Route::get('/event/{id}', [HomeController::class, 'evenListeneerShip'])->name('evenListeneerShip');
+// Route::get('/cache/{id}', [HomeController::class, 'cacheRedis'])->name('cacheRedis');
+// Route::get('/cache', [CachesController::class, 'index'])->name('cacheRedisIndex');
 
-Route::get('/database-relationship/one-one', [DatabaseController::class, 'relationOneOne'])->name('relationDbOneOne');
-Route::get('/database-relationship/one-many', [DatabaseController::class, 'relationOneMany'])->name('relationDbOneMany');
+// Route::get('/database-relationship/one-one', [DatabaseController::class, 'relationOneOne'])->name('relationDbOneOne');
+// Route::get('/database-relationship/one-many', [DatabaseController::class, 'relationOneMany'])->name('relationDbOneMany');
+
+// $prefixAdmin = 'admin';
+// // Route::group(['middleware' => ['auth', 'permission.admin'], 'prefix' => $prefixAdmin], function () {
+// // Route::group(['middleware' => ['auth'], 'prefix' => $prefixAdmin], function () {
+// Route::group(['prefix' => $prefixAdmin], function () {
+
+
+//     // Routes for admin
+//     // echo '<h3>Die is Called - 23</h3>';die;
+//     include_once __DIR__ . '/admin.php';
+// });
 
 
 
@@ -32,3 +44,11 @@ Route::get('/database-relationship/one-many', [DatabaseController::class, 'relat
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
